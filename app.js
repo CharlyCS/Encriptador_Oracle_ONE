015f944f3c02b856bee70cbc7ffaed837d904408
CharlyCS <ccoronels@uni.pe>
2024-08-24 00:07:10 -0500

    encryption and decryption finished

--- app.js
+++ app.js
@@ -2,39 +2,33 @@ const clave = 3;
 
 function encriptar(){
     const variable = document.getElementById('input').value;
-    let textoCifrado = '';
-
+    let textoencriptado = '';
+    // Se considera letras minusculas y sin acento
     for (let i = 0; i < variable.length; i++) {
         let charCode = variable.charCodeAt(i);
 
-        if (charCode >= 65 && charCode <= 90) {
-            textoCifrado += String.fromCharCode(((charCode - 65 + clave) % 26) + 65);
-        } else if (charCode >= 97 && charCode <= 122) {
-            textoCifrado += String.fromCharCode(((charCode - 97 + clave) % 26) + 97);
+        if (charCode >= 97 && charCode <= 122) {
+            textoencriptado += String.fromCharCode(((charCode - 97 + clave) % 26) + 97);
         } else {
-            textoCifrado += texto[i];
+            textoencriptado += texto[i];
         }
     }
-
-    document.getElementById("textoCifrado").value = textoCifrado;
+    document.getElementById("output").innerHTML = `<p>${textoencriptado}</p>`;
 }
 
 function desencriptar() {
-    const textoCifrado = document.getElementById("textoCifradoInput").value;
-    let textoDesencriptado = '';
+    const textoCifrado = document.getElementById("input").value;
+    let textodesencriptado = '';
 
     for (let i = 0; i < textoCifrado.length; i++) {
         let charCode = textoCifrado.charCodeAt(i);
 
-        // Solo desplazar letras mayúsculas y minúsculas
-        if (charCode >= 65 && charCode <= 90) {
-            textoDesencriptado += String.fromCharCode(((charCode - 65 - clave + 26) % 26) + 65);
-        } else if (charCode >= 97 && charCode <= 122) {
-            textoDesencriptado += String.fromCharCode(((charCode - 97 - clave + 26) % 26) + 97);
+        if (charCode >= 97 && charCode <= 122) {
+            textodesencriptado += String.fromCharCode(((charCode - 97 - clave + 26) % 26) + 97);
         } else {
-            textoDesencriptado += textoCifrado[i]; // Otros caracteres no se modifican
+            textodesencriptado += textoCifrado[i];
         }
     }
 
-    document.getElementById("textoDesencriptado").value = textoDesencriptado;
+    document.getElementById("output").innerHTML = `<p>${textodesencriptado}</p>`;
 }
